9933230ed15a87825e1463857998c6794802a079
Ljubomir <ljubomir0001@gmail.com>
2025-02-12 16:41:32 +0100

    end of first tutorial

---
+++ movies/views.py
@@ -0,0 +1,35 @@
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from .models import Movie
+
+def movies(request):
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies': data})
+
+def home(request):
+    return HttpResponse("homepage")
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie':data})
+
+def add(request):
+
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+
+    return render(request, 'movies/add.html')
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id).delete()
+    except:
+        raise Http404('movie doesnt exist')
+
+    movie.delete()                  
+    return HttpResponseRedirect('/movies')
